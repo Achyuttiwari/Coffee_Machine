9f0d5338e27a37f3915000797015487e4ad67d85
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-04-10 19:20:28 +0530

    enum status

--- Coffee Machine/task/src/machine/Stage4/coffeeMachine.java
+++ Coffee Machine/task/src/machine/Stage4/coffeeMachine.java
@@ -7,4 +7,11 @@ public class coffeeMachine {
     static int beans = 120;
     static int cups = 9;
     static int money = 550;
+
+    static Boolean exit = false;
+
+    enum Status {
+        CHOOSING, BUYING, FILLING, TAKING, REMAINING, EXITING
+    }
+
 }
