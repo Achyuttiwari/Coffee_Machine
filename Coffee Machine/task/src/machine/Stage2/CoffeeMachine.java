cd62bbde7767042cffe5875cd56de31c8a62014a
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-04-10 18:18:37 +0530

    Scanner in used for helping to get input from user.

---
+++ Coffee Machine/task/src/machine/Stage2/CoffeeMachine.java
@@ -0,0 +1,11 @@
+
+package machine.Stage2;
+
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
