0a2f8e0aef95541a8b3341b2004a7ad45442a5d1
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-04-10 18:58:20 +0530

    taking all the variable static because in this we need to write a static function.

---
+++ Coffee Machine/task/src/machine/Stage4/coffeeMachine.java
@@ -0,0 +1,10 @@
+
+package machine.Stage4;
+
+public class coffeeMachine {
+    static int water = 1200;
+    static int milk = 540;
+    static int beans = 120;
+    static int cups = 9;
+    static int money = 550;
+}
