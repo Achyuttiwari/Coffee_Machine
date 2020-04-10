2e46cf2bf6bd23f167c03b342a728290cd97f590
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-04-10 18:53:17 +0530

    if condition used to ensure the quantity of all things is being perfect for making the coffee.

--- Coffee Machine/task/src/machine/Stage3/coffeeMachine.java
+++ Coffee Machine/task/src/machine/Stage3/coffeeMachine.java
@@ -2,7 +2,7 @@ package machine.Stage3;
 
 import java.util.Scanner;
 
-public class CoffeeMachine {
+class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
