b42977a15c5ea145b08d21845a3c1d6ed20e7782
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-04-10 18:21:24 +0530

    Amount of each ingredient / 1 cup

--- Coffee Machine/task/src/machine/Stage2/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/Stage2/CoffeeMachine.java
@@ -7,5 +7,10 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //amount of each ingredients per 1 cup
+
+        int WATER = 200;
+        int MILK = 50;
+        int BEANS = 15;
     }
 }
